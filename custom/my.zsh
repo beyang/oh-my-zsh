1a1deb6aa635c73d298354764d83abe21ec97c77
Beyang Liu <beyang.liu@gmail.com>
2013-06-06 19:45:59 -0700

    get rid of unnecessary pythonpath set (which can mess up virtualenv)

--- custom/my.zsh
+++ custom/my.zsh
@@ -6,9 +6,6 @@ export SHELL=$(which zsh)
 alias virgil='cowsay -f $(ls /usr/local/share/cows | gsort -R | head -1) "$(fortune)"'
 alias diff='colordiff'
 
-# for opencv python module
-export PYTHONPATH="/usr/local/lib/python2.7/site-packages:$PYTHONPATH"
-
 export ZSH_THEME="superjarin"
 
 unsetopt share_history
