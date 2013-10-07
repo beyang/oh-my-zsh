6d002c637659c018c099f475ff30357c4fe817ea
Beyang Liu <beyang.liu@gmail.com>
2013-10-07 10:01:51 -0700

    aliases

---
+++ custom/alias.zsh
@@ -0,0 +1,10 @@
+# Fetch
+alias gfo="git fetch origin"
+alias gco="git checkout"
+alias gcm="git commit"
+alias gst="git status"
+alias gl="git log"
+alias gb="git branch"
+
+# Make
+alias mkae="make"
